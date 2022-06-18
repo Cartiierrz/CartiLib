ccdf7a441af0713edce47d93eee4a9cfeb6bc4d5
Cartiierrz <107292246+Cartiierrz@users.noreply.github.com>
2022-06-18 14:46:14 -0500

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using CartiLib;
+using CartiLib.Interface;
+using CartiLib.Title;
+using System.Threading;
+using CartiLib.Configuration;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            SettingsMaker.Load();
+            SettingsModel.ReadSettings();
+            AsciiVariables.AppName = "CartiLib";
+            TitleMaker.CreateBasic("CartiLib", "Home", "Carti#0145");
+            AsciiMaker.Init("Big.flf");
+            InterfaceMaker.CreateNotification("Developed by Carti#0145");
+            Thread.Sleep(-1);
+        }
+    }
+}
